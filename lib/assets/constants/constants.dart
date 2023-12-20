810ba21cc0b71e4f6265f4cf133b35f4f43b3525
MogaDenis <moga_denis@yahoo.com>
2023-12-20 21:29:50 +0200

    Pushing directly to main :) Installed dependencies for google fonts

---
+++ lib/assets/constants/constants.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+// Add to this class all colors used.
+class ColorConstants {
+  static const Color black = Colors.black;
+  static const Color white = Colors.white;
+  static const Color deepPurple = Colors.deepPurple;
+
+  // and so on...
+}
+
+
+// Add to this all fonts used.
+class FontConstants {
+  static String? roboto = GoogleFonts.roboto().fontFamily; 
+  static String? lato = GoogleFonts.lato().fontFamily;
+  static String? luxuriousRoman = GoogleFonts.luxuriousRoman().fontFamily;
+  static String? openSans = GoogleFonts.openSans().fontFamily;
+
+  // and so on...
+}
