4bb0db03a6f4720e761a3ae81633b1d3a0d155ea
MogaDenis <moga_denis@yahoo.com>
2024-01-30 14:15:21 +0200

    A little bit of refactoring

--- lib/screens/multiple_answer_question.dart
+++ lib/screens/multiple_answer_question.dart
@@ -25,16 +25,20 @@ class MultipleAnswerQuestion extends StatefulWidget {
 class _MultipleAnswerQuestionState extends State<MultipleAnswerQuestion> {
   void _checkAnswer(Answer answer) {
     if (!answer.isCorrect) {
-      ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
-        content: Text("Wrong!"),
-        duration: Duration(seconds: 1),
-      ));
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(
+          content: Text("Wrong!"),
+          duration: Duration(seconds: 1),
+        ),
+      );
       return;
     }
-    ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
-      content: Text("Correct!"),
-      duration: Duration(seconds: 1),
-    ));
+    ScaffoldMessenger.of(context).showSnackBar(
+      const SnackBar(
+        content: Text("Correct!"),
+        duration: Duration(seconds: 1),
+      ),
+    );
   }
 
   Widget _question() {
@@ -54,65 +58,74 @@ class _MultipleAnswerQuestionState extends State<MultipleAnswerQuestion> {
 
   Widget _answersGroup(Answer answer1, Answer answer2) {
     return Row(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: <Widget>[
-          ElevatedButton(
-              onPressed: () {
-                _checkAnswer(answer1);
-              },
-              child: Text(
-                answer1.answerText,
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: <Widget>[
+        ElevatedButton(
+          onPressed: () {
+            _checkAnswer(answer1);
+          },
+          child: Text(
+            answer1.answerText,
+            style: const TextStyle(
+              color: Color(0xFF527DAA),
+              fontSize: 25.0,
+              fontWeight: FontWeight.bold,
+              fontFamily: 'OpenSans',
+            ),
+            textAlign: TextAlign.center,
+          ),
+        ),
+        ElevatedButton(
+            onPressed: () {
+              _checkAnswer(answer2);
+            },
+            child: Text(answer2.answerText,
                 style: const TextStyle(
                   color: Color(0xFF527DAA),
                   fontSize: 25.0,
                   fontWeight: FontWeight.bold,
                   fontFamily: 'OpenSans',
                 ),
-                textAlign: TextAlign.center,
-              )),
-          ElevatedButton(
-              onPressed: () {
-                _checkAnswer(answer2);
-              },
-              child: Text(answer2.answerText,
-                  style: const TextStyle(
-                    color: Color(0xFF527DAA),
-                    fontSize: 25.0,
-                    fontWeight: FontWeight.bold,
-                    fontFamily: 'OpenSans',
-                  ),
-                  textAlign: TextAlign.center)),
-        ]);
+                textAlign: TextAlign.center)),
+      ],
+    );
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        resizeToAvoidBottomInset: true,
-        body: GestureDetector(
-            onTap: () => FocusScope.of(context).unfocus(),
-            child: Stack(children: <Widget>[
-              CustomContainers.backgroundContainer,
-              SizedBox(
-                  height: double.infinity,
-                  child: SingleChildScrollView(
-                    padding: const EdgeInsets.fromLTRB(20, 100, 20, 10),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
+      resizeToAvoidBottomInset: true,
+      body: GestureDetector(
+        onTap: () => FocusScope.of(context).unfocus(),
+        child: Stack(
+          children: <Widget>[
+            CustomContainers.backgroundContainer,
+            SizedBox(
+              height: double.infinity,
+              child: SingleChildScrollView(
+                padding: const EdgeInsets.fromLTRB(20, 100, 20, 10),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    _question(),
+                    const Image(
+                        image: AssetImage(ImagesConstants.fullWolfImage)),
+                    const SizedBox(height: 20.0),
+                    Column(
                       children: <Widget>[
-                        _question(),
-                        const Image(
-                            image: AssetImage(ImagesConstants.fullWolfImage)),
+                        _answersGroup(widget.answer1, widget.answer2),
                         const SizedBox(height: 20.0),
-                        Column(children: <Widget>[
-                          _answersGroup(widget.answer1, widget.answer2),
-                          const SizedBox(height: 20.0),
-                          _answersGroup(widget.answer3, widget.answer4),
-                        ])
+                        _answersGroup(widget.answer3, widget.answer4),
                       ],
                     ),
-                    // const SizedBox(height: 150.0),
-                  ))
-            ])));
+                  ],
+                ),
+                // const SizedBox(height: 150.0),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
